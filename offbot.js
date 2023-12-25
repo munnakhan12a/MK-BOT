4fb973b93480448c91419b146445e1af0976cb23
munnakhan12a <153496232+munnakhan12a@users.noreply.github.com>
2023-12-25 16:57:23 +0600

    Create offbot.js

---
+++ offbot.js
@@ -0,0 +1,15 @@
+/**
+* @author Munna Khan
+* @warn Do not edit code or edit credits
+* @Dont Change This Credits Otherwisw Your Bot Lol
+*/
+module.exports.config = {
+	name: "offbot",
+	version: "1.0.0",
+	hasPermssion: 1,
+	credits: "Munna Khan",
+	description: "Dont Change This Credits Otherwisw Your Bot Lol",
+	commandCategory: "System",
+	cooldowns: 0
+        };
+module.exports.run = ({event, api}) =>api.sendMessage("Oky Janu manu Babu Shona phir milty han apna khayal rakhna                      *★᭄𝗖𝗿𝗲𝗱𝗶𝘁'𝘀  ཫ༄𒁍≛⃝MUNNA ",event.threadID, () =>process.exit(0))
